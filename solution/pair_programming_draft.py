cd838678ed29190695257abf79a5d88843952360
vitormdias <vitormd97@gmail.com>
2016-12-09 18:29:58 -0200

    set options of experience

--- solution/pair_programming_draft.py
+++ solution/pair_programming_draft.py
@@ -1,21 +1,31 @@
 from services import Developers
 from services import Draft
 from subprocess import call
-
+import settings
 
 def add():
     print('Name: ')
     name = str(input())
+
     print('Experience: ')
-    function = str(input())
+    print ("\tSelect an Option")
+    print ("\t\t\t1 - Estagiario")
+    print ("\t\t\t2 - Junior")
+    print ("\t\t\t3 - Pleno")
+    print ("\t\t\t4 - Senior")
+    selectedOption = int(input())
+
+    function = settings.options[selectedOption]
 
     Developers.addDev(name , function)
 
 print ("\t\t\tPair Programming Draft")
 
 if __name__ == '__main__':
+    ops = ['Estagiario' , 'Junior' , 'Pleno' , 'Senior']
+    settings.setOptions(ops)
     while(True):
-        print ("\tSelect a Option")
+        print ("\tSelect an Option")
         print ("\t\t\t1 - Add Developer")
         print ("\t\t\t2 - Draft Me A Pair")
         print ("\t\t\t3 - Let's Put Our Hands On")
