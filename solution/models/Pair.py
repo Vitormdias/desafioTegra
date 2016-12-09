57886c7c472b4cce43c87cda6a6a958d1c4c7601
vitormdias <vitormd97@gmail.com>
2016-12-09 18:40:06 -0200

    Printing updated

--- solution/models/Pair.py
+++ solution/models/Pair.py
@@ -4,4 +4,4 @@ class Pair():
         self.navigator = navigator
 
     def presentation(self):
-        return "Driver: {0!s} Navigator: {1!s}".format(self.driver.name , self.navigator.name)
+        return "Driver: {0!s} - Navigator: {1!s}".format(self.driver.name , self.navigator.name)
