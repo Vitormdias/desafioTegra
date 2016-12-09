c7cbedb4927c7cad18b7d4d3271b236465bc1c64
vitormdias <vitormd97@gmail.com>
2016-12-09 00:39:00 -0200

    Downcase in attr

--- solution/classes/Developer.py
+++ solution/classes/Developer.py
@@ -1,7 +1,7 @@
 class Developer():
     def __init__ (self , name,experience):
-        self.name = name
-        self.experience = experience
+        self.name = name.lower()
+        self.experience = experience.lower()
 
     def presentation(self):
         return "Ola sou {0!s} e sou um {1!s}".format(self.name ,self.experience)
