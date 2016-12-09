d3cbf8ca0fe71534199b69c68613de88041ac5af
vitormdias <vitormd97@gmail.com>
2016-12-08 23:45:09 -0200

    Add developer implemented

---
+++ solution/controller/Developers.py
@@ -0,0 +1,12 @@
+from classes.Developer import Developer
+
+developers = []
+
+def addDev(name , function):
+    developer = Developer(name , function)
+
+    developers.append(developer)
+
+def show():
+    for dev in developers:
+        print (dev.presentation())
