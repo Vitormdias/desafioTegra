9b0bf09090629c0fb9b44bcf5847d6cb065ae41b
vitormdias <vitormd97@gmail.com>
2016-12-10 11:52:26 -0200

    Experience saved as int now

--- solution/models/Developer.py
+++ solution/models/Developer.py
@@ -1,7 +1,9 @@
+import settings
+
 class Developer():
-    def __init__ (self , name,experience):
+    def __init__ (self , name, experience):
         self.name = name.capitalize()
-        self.experience = experience.capitalize()
+        self.experience = experience
 
     def presentation(self):
-        return "Name: {0!s} Experience: {1!s}".format(self.name ,self.experience)
+        return "Name: {0!s} Experience: {1!s}".format(self.name , settings.options[self.experience])
